3cce11d3f41fee4d3b896feeb6da7e46a027422a
Daniel Sanchez <sanchez@csail.mit.edu>
2015-02-24 20:22:09 -0500

    [ctxt] Better MT test

--- ctxt/mttest.cpp
+++ ctxt/mttest.cpp
@@ -16,27 +16,41 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <cstddef>
 
+uint64_t baseiters;
 volatile int x;
 
 void* worker(void* arg) {
     uint64_t v = (uintptr_t)arg;
-    printf("%ld\n", v);
-    for (uint32_t i = 0; i < (v+1)*100000; i++) {
+    printf("worker %ld\n", v);
+    for (uint32_t i = 0; i < (v+1)*baseiters; i++) {
         __sync_fetch_and_add(&x, 1);
     }
 }
 
-int main() {
-    pthread_t th;
-    pthread_create(&th, nullptr, worker, (void*)1);
-    worker((void*)2);
-    pthread_join(th, nullptr);
+int main(int argc, const char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <nthreads> <baseiters>\n", argv[0]);
+    }
+    uint32_t nthreads = atoi(argv[1]);
+    baseiters = atoi(argv[2]);
+    assert(nthreads > 0);
+    printf("Running with %d threads, %ld base iters\n", nthreads, baseiters);
+    
+    pthread_t th[nthreads];
+    for (uint32_t i = 1; i < nthreads; i++) {
+        pthread_create(&th[i], nullptr, worker, (void*)(uintptr_t)i);
+    }
+    worker((void*)0);
+    for (uint32_t i = 1; i < nthreads; i++) {
+        pthread_join(th[i], nullptr);
+    }
     printf("x: %ld\n", x);
     return 0;
 }
