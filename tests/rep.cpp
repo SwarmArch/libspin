8a8845d782a1fe425231efc570da89fd57c14645
Daniel Sanchez <sanchez@csail.mit.edu>
2015-03-19 11:01:54 -0400

    [tests] Add rep test
    At some point, spin-fast failed if we added a switchcall before a
    rep-prefixed instruction. This is a minimal test that would expose this
    bug, using both rep stosb and repnz scasb. I thought Pin might be using
    internal context registers to track the state of rep instructions, but
    this seems to work fine, all repped instructions are actually
    interruptible, and their internal state is in regular registers. So
    probably this happened because of a bug that mishandled those registers.

---
+++ tests/rep.cpp
@@ -0,0 +1,50 @@
+/** $lic$
+ * Copyright (C) 2015 by Massachusetts Institute of Technology
+ *
+ * This file is part of libspin.
+ *
+ * libspin is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2.
+ *
+ * libspin is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    const char* s = "Verify: OK\n";
+    char buf[1024];
+
+    strcpy(buf, "Verify: Incorrect REP STOSB\n");
+
+    asm volatile("rep stosb" :: "c"(strlen(s)-1), "a"(42), "D"(buf) : "cc", "memory");
+
+    printf(buf);
+
+    memset(buf, 0, sizeof(buf));
+    strcpy(buf, "REPNZ SCASB finds the first instance of a @ byte\n");
+    int out = 1000;
+    asm volatile("repnz scasb" : "=c"(out) : "c"(100), "a"('@'), "D"(buf) : "cc", "memory");
+    int pos = 100 - out - 1;
+    printf("%d %c\n", pos, buf[pos]);
+
+    if (buf[pos] == '@') {
+        printf(s);
+    }
+
+    return 0;
+}
+
+
+
+
+
